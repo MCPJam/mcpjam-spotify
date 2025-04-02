d3b17d0712e8ffd93f1153573ba010f9d8dbfce9
Matthew Wang <matt8p@outlook.com>
2025-04-02 16:10:02 -0700

    t

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,4 @@
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 
-// TODO: Make these configurable
-export const api = SpotifyApi.withClientCredentials(
-  "530cc8f8c0564ef48875b014e32bc987",
-  "80e3e74b5a88479292a5c508dbc0a0e6"
-);
+// TODO: Configure these env keys
+export const api = SpotifyApi.withClientCredentials("123", "123");
